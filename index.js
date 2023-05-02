9ed7d7194743b7d3a424f62c0167ace724fcae81
git4novel <novelsylhet@gmail.com>
2023-05-02 14:51:07 +0600

    after adding data

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 
+const cors = require('cors')
+
+const chefData = require('./chefdata.json')
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.send('this is awesome in server')
