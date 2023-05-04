cfc95d79acd7317892c60c53ebf7a81a983734a8
git4novel <novelsylhet@gmail.com>
2023-05-04 13:58:23 +0600

    provable final commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 
 const chefData = require('./chefdata.json')
 const favDishes = require('./favdishes.json')
+const cheSpecialDishes = require('./chefSpecialRecipes.json')
 
 const app = express();
 
@@ -12,10 +13,26 @@ app.get('/', (req, res)=>{
     res.send(chefData)
 })
 
+
+app.get('/chefDishes/:id', (req, res) =>{
+    const id = req.params.id;
+    // console.log(id);
+    const filterRecipes = cheSpecialDishes.filter(d =>d.chef_id == id )
+    res.send(filterRecipes)
+    // res.send(cheSpecialDishes)
+    
+})
+
 app.get('/favdishes', (req, res) =>{
     res.send(favDishes);
 })
 
+app.get('/:id', (req, res)=>{
+    const id = req.params.id;
+    // console.log(id);
+    const selectedChef = chefData.find(c => c._id == id);
+    res.send(selectedChef)
+}) 
 app.listen(5000, () =>{
     console.log('Port is running here on 5000');
 })
